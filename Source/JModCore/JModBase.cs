b5799e45af0dfd447b19b65f3ac19ce77dc97dd8
James <james@epicguru.co.uk>
2021-01-08 16:34:45 +0100

    Added references to Rimworld assemblies.

--- Source/JModCore/JModBase.cs
+++ Source/JModCore/JModBase.cs
@@ -1,10 +1,15 @@
-﻿namespace JModCore
+﻿using Verse;
+
+namespace JModCore
 {
     /// <summary>
-    /// Alternative to Rimworld's Mod class, adding more functionality and
+    /// Alternative to Rimworld's <see cref="Mod"/> class, adding more functionality and
     /// utilities.
     /// </summary>
-    public abstract class JModBase
+    public abstract class JModBase : Mod
     {
+        protected JModBase(ModContentPack content) : base(content)
+        {
+        }
     }
 }
